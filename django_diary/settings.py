00e5900deb4355e09db570780e657917ad43c2d8
George Repole <grepole121@gmail.com>
2021-03-09 01:55:16 +0000

    Made register functionality and forms on /register

--- django_diary/settings.py
+++ django_diary/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'diary.apps.DiaryConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
